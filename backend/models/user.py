79f6778138bc68b7cb575d53e303c63c482938cf
Mikołaj Hoffmann <miki.hoffmann@op.pl>
2024-12-18 23:16:20 +0100

    Dodanie first_name i last_name

--- backend/models/user.py
+++ backend/models/user.py
@@ -9,6 +9,8 @@ class User(Base):
     __tablename__ = 'users' #Nazwa tabeli w bazie danych
     
     id = Column(Integer, primary_key=True, index=True)      #
+    first_name = Column(String(255), unique=False, nullable=False)
+    last_name = Column(String(255), unique=False, nullable=False)
     username = Column(String(255), unique=True, nullable=False)  #
     email = Column(String(255), unique=True, nullable=False)     #   Nazwy kolumn w bazie
     hashed_password = Column(String(255), nullable=False)        #
@@ -16,6 +18,8 @@ class User(Base):
 class UserBase(BaseModel):
     username: str
     email: EmailStr
+    first_name: str
+    last_name: str
 
 class UserCreate(UserBase):
     password: str
