09d64f384183248cf6a56d8d307b87359506b45a
Mikołaj Hoffmann <miki.hoffmann@op.pl>
2024-12-22 16:21:46 +0100

    Poprawa z komunikacji z backend

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BACKEND_URL } from "../../Components/config";
+import { BACKEND_URL2, BACKEND_URL} from "../../Components/config";
 import ThemeSwitch from '../../Components/ThemeSwitch/ThemeSwitch';
 
 import "./Register.scss";
@@ -75,8 +75,8 @@ const Register = () => {
     }
 
     const userData = {
-      firstName: userDetails.firstName,
-      lastName: userDetails.lastName,
+      first_name: userDetails.firstName,
+      last_name: userDetails.lastName,
       username: userDetails.username,
       email: userDetails.email,
       password: userDetails.password,
@@ -84,7 +84,7 @@ const Register = () => {
 
     try {
       //1, Sprawdzanie 
-      const checkResponse = await fetch(`${BACKEND_URL}/users/check`, {
+      const checkResponse = await fetch(`${BACKEND_URL2}/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -98,7 +98,7 @@ const Register = () => {
 
 
       //2. Rejestracja użytkownika, jeśli wszystko jest w porządku.
-      const response = await fetch(`${BACKEND_URL}/users`, {
+      const response = await fetch(`${BACKEND_URL2}/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
