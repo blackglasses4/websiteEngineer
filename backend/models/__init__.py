ed66575c35ae64e16b1b5e1dbdec37e5e45b9f3d
Mikołaj Hoffmann <miki.hoffmann@op.pl>
2024-12-29 19:12:40 +0100

    Dodanie orders

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,6 @@
 from .product import Product
 from .product_images import ProductPicture
 from .user import User
+from .orders import Order
 
-__all__ = ["Product", "ProductPicture", "User"]
+__all__ = ["Product", "ProductPicture", "User", "Order"]
